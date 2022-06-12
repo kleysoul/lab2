4d6335dfb991541c78dcf6b9e05faac79649ce3d
kleysoul <silk.chudinovski@gmal.com>
2022-06-12 18:23:32 +0300

    local change

--- project/main.cpp
+++ project/main.cpp
@@ -4,5 +4,7 @@ int main()
 {
     std::cout << "Початковий файл проекту" << std::endl;
     std::cout << "Зміна 1" << std::endl;
+    std::cout << "Локальна зміна" << std::endl;
     return 0; 
+
 }
